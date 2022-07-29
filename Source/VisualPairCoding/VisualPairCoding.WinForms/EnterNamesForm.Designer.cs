50717652dd10afb5b545fc81f84e43cde0b195d2
Stefan Hoffmann <stho32github@gmail.com>
2022-07-29 10:45:54 -0700

    [Kanban 43] Draft for first form

---
+++ Source/VisualPairCoding/VisualPairCoding.WinForms/EnterNamesForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace VisualPairCoding.WinForms
+{
+    partial class EnterNamesForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.participant1Textbox = new System.Windows.Forms.TextBox();
+            this.participant2Textbox = new System.Windows.Forms.TextBox();
+            this.participant3Textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.participant4Textbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.participant5Textbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Participant 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Participant 2";
+            // 
+            // participant1Textbox
+            // 
+            this.participant1Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.participant1Textbox.Location = new System.Drawing.Point(90, 17);
+            this.participant1Textbox.Name = "participant1Textbox";
+            this.participant1Textbox.Size = new System.Drawing.Size(176, 23);
+            this.participant1Textbox.TabIndex = 2;
+            // 
+            // participant2Textbox
+            // 
+            this.participant2Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.participant2Textbox.Location = new System.Drawing.Point(90, 46);
+            this.participant2Textbox.Name = "participant2Textbox";
+            this.participant2Textbox.Size = new System.Drawing.Size(176, 23);
+            this.participant2Textbox.TabIndex = 3;
+            // 
+            // participant3Textbox
+            // 
+            this.participant3Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.participant3Textbox.Location = new System.Drawing.Point(90, 75);
+            this.participant3Textbox.Name = "participant3Textbox";
+            this.participant3Textbox.Size = new System.Drawing.Size(176, 23);
+            this.participant3Textbox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Participant 3";
+            // 
+            // participant4Textbox
+            // 
+            this.participant4Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.participant4Textbox.Location = new System.Drawing.Point(90, 104);
+            this.participant4Textbox.Name = "participant4Textbox";
+            this.participant4Textbox.Size = new System.Drawing.Size(176, 23);
+            this.participant4Textbox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Participant 4";
+            // 
+            // participant5Textbox
+            // 
+            this.participant5Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.participant5Textbox.Location = new System.Drawing.Point(90, 133);
+            this.participant5Textbox.Name = "participant5Textbox";
+            this.participant5Textbox.Size = new System.Drawing.Size(176, 23);
+            this.participant5Textbox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Participant 5";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(184, 162);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 23);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(166, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Number of minutes per round";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(110, 197);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 12;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(191, 197);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 13;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // EnterNamesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 232);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.participant5Textbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.participant4Textbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.participant3Textbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.participant2Textbox);
+            this.Controls.Add(this.participant1Textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EnterNamesForm";
+            this.Text = "Enter the names";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox participant1Textbox;
+        private TextBox participant2Textbox;
+        private TextBox participant3Textbox;
+        private Label label3;
+        private TextBox participant4Textbox;
+        private Label label4;
+        private TextBox participant5Textbox;
+        private Label label5;
+        private NumericUpDown numericUpDown1;
+        private Label label6;
+        private Button startButton;
+        private Button closeButton;
+    }
+}
---
+++ Source/VisualPairCoding/VisualPairCoding.WinForms/EnterNamesForm.cs
@@ -0,0 +1,15 @@
+namespace VisualPairCoding.WinForms
+{
+    public partial class EnterNamesForm : Form
+    {
+        public EnterNamesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
