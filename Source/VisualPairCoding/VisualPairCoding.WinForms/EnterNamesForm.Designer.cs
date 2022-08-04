e6a282c3553f7f67da06c996b2965b78bce9355c
naseif <d2mds21@gmail.com>
2022-08-04 15:41:16 +0200

    Added icon for the app

--- Source/VisualPairCoding/VisualPairCoding.WinForms/EnterNamesForm.Designer.cs
+++ Source/VisualPairCoding/VisualPairCoding.WinForms/EnterNamesForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnterNamesForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.participant1Textbox = new System.Windows.Forms.TextBox();
@@ -192,6 +193,7 @@
             this.Controls.Add(this.participant1Textbox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "EnterNamesForm";
             this.Text = "Setup Pair/Mob Coding";
             ((System.ComponentModel.ISupportInitialize)(this.minutesPerRoundNumericUpDown)).EndInit();
