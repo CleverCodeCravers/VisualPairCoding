eada4cecd0b1cc588ba26372d80101529c0a429a
Stefan Hoffmann <stho32github@gmail.com>
2022-08-19 18:40:54 +0200

    fixed typo

--- Scripts/Set-Version-Number.ps1
+++ Scripts/Set-Version-Number.ps1
@@ -9,4 +9,4 @@ Set-Location ..\Source\VisualPairCoding\VisualPairCoding.BL\AutoUpdates
 $versionInformation = Get-Content VersionInformation.cs -Raw -Encoding UTF8
 $versionInformation = $versionInformation.Replace("$$VERSION$$", $NewVersion)
 
-$versionInformation | Set-Content VersionInfomration.cs -Encoding UTF8
+$versionInformation | Set-Content VersionInformation.cs -Encoding UTF8
