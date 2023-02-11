45c141f9d05c088da0dbd068fe96fb29f6c052ab
naseif <d2mds21@gmail.com>
2023-02-11 19:14:25 +0100

    Added loading and saving to the new Ui

--- Source/VisualPairCoding/VisualPairCoding.AvaloniaUI/EnterNamesWindow.axaml.cs
+++ Source/VisualPairCoding/VisualPairCoding.AvaloniaUI/EnterNamesWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System;
+using System.Collections.Generic;
+using VisualPairCoding.Infrastructure;
 
 namespace VisualPairCoding.AvaloniaUI
 {
@@ -11,9 +13,144 @@ namespace VisualPairCoding.AvaloniaUI
             InitializeComponent();
         }
         
-        public void ClickDenHandler(object? sender, RoutedEventArgs args)
+
+        public void CloseWindow(object? sender, RoutedEventArgs args)
         {
-            Console.WriteLine("Hello");
+            Close();
+        }
+
+        public async void StartForm(object? sender, RoutedEventArgs args)
+        {
+
+            var participants = GetParticipants();
+            Console.WriteLine(participants.ToArray());
+
+        }
+
+        public void LoadSessionIntoGui(string fileName)
+        {
+            try
+            {
+                var session = SessionConfigurationFileHandler.Load(fileName);
+
+                LoadSessionIntoGui(session);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error Loading configuration File: " + ex.Message);
+            }
+        }
+
+        public void LoadSessionIntoGui(SessionConfiguration session)
+        {
+            participant1.Focus();
+
+            for (var i = 1; i <= 10; i++)
+            {
+                var control = this.FindControl<TextBox>("participant" + i); 
+
+                if (control != null)
+                {
+                    control.Text = "";
+                }
+            }
+
+            for (var i = 1; i <= session.Participants.Count; i++)
+            {
+                var control = this.FindControl<TextBox>("participant" + i);
+
+                if (control != null)
+                {
+                    control.Text = session.Participants[i - 1].Trim();
+                }
+            }
+
+            minutesPerTurn.Value = session.SessionLength;
+        }
+
+        private void AddParticipantIfAvailable(string name, List<string> participants)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+                participants.Add(name);
+        }
+
+        private List<string> GetParticipants()
+        {
+            var participants = new List<string>();
+            AddParticipantIfAvailable(participant1.Text, participants);
+            AddParticipantIfAvailable(participant2.Text, participants);
+            AddParticipantIfAvailable(participant3.Text, participants);
+            AddParticipantIfAvailable(participant4.Text, participants);
+            AddParticipantIfAvailable(participant5.Text, participants);
+            AddParticipantIfAvailable(participant6.Text, participants);
+            AddParticipantIfAvailable(participant7.Text, participants);
+            AddParticipantIfAvailable(participant8.Text, participants);
+            AddParticipantIfAvailable(participant9.Text, participants);
+            AddParticipantIfAvailable(participant10.Text, participants);
+
+            return participants;
+        }
+
+        public async void LoadSessionConfiguration(object? sender, RoutedEventArgs args)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filters = new List<FileDialogFilter>()
+            {
+                new FileDialogFilter()
+                {
+                    Name="Json",
+                    Extensions=new List<string>()
+                    {
+                        "json"
+                    }
+                }
+            };
+            openFileDialog.Title = "config.json";
+            openFileDialog.AllowMultiple = false;
+
+            var result = await openFileDialog.ShowAsync(this);
+
+            if (result?.Length > 0)
+            {
+                LoadSessionIntoGui(result[0]);
+            }
+
+        }
+        public async void SaveSessionConfiguration(object? sender, RoutedEventArgs args)
+        {
+            var participants = GetParticipants();
+
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filters = new List<FileDialogFilter>()
+            {
+                new FileDialogFilter()
+                {
+                    Name="Json",
+                    Extensions=new List<string>()
+                    {
+                        "json"
+                    }
+                }
+            };
+            saveFileDialog.Title = "config.json";
+
+
+            var result = await saveFileDialog.ShowAsync(this);
+
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                try
+                {
+                    SessionConfigurationFileHandler.Save(result, new SessionConfiguration(participants, (int)minutesPerTurn.Value));
+                    //MessageBox.Show("Session Configuration saved successfully!");
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show("Error saving configuration File: " + ex.Message);
+                }
+      
+            }
         }
     }
 }
