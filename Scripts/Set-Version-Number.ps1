7390403b9046840399fa8b204c6cdb3dc0c39bbc
naseif <d2mds21@gmail.com>
2023-09-17 20:03:40 +0200

    remove refs/tags

--- Scripts/Set-Version-Number.ps1
+++ Scripts/Set-Version-Number.ps1
@@ -2,11 +2,13 @@ Param(
     [string]$NewVersion
 )
 
+$versionNumber = $NewVersion -replace '^refs/tags/', ''
+
 Set-Location $PSScriptRoot
 
 Set-Location ..\Source\VisualPairCoding\VisualPairCoding.BL
 
 $versionInformation = Get-Content VersionInformation.cs -Raw -Encoding UTF8
-$versionInformation = $versionInformation.Replace('$$VERSION$$', $NewVersion)
+$versionInformation = $versionInformation.Replace('$$VERSION$$', $versionNumber)
 
 $versionInformation | Set-Content VersionInformation.cs -Encoding UTF8
